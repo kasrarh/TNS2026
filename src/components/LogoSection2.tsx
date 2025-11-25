eeded7a4980c8910d6524b2a50abd0f880b239a8
kasrarh <55912648+kasrarh@users.noreply.github.com>
2025-11-25 15:39:41 -0500

    Add new sponsor logo section with template image
    Introduced LogoSection2 component displaying a sponsor template image and updated the main page to use this new section instead of the previous LogosSection. Added sponsor-template.png image asset.

---
+++ src/components/LogoSection2.tsx
@@ -0,0 +1,12 @@
+export default function LogosSection2() {
+
+    return (
+        <section className="sponsors" id="sponsors">
+            <div className="container">
+                <img src="/sponsors/sponsor-template.png" alt="" width={'100%'} />
+            </div>
+        </section>
+    );
+}
+
+
