d0983c0ac7865615a6eda56c685362e6731aff15
suluxan <suluxan.m@gmail.com>
2026-01-22 14:04:12 -0500

    new sponsors added

--- src/components/LogoSection2.tsx
+++ src/components/LogoSection2.tsx
@@ -3,7 +3,7 @@ export default function LogosSection2() {
     return (
         <section className="sponsors" id="sponsors">
             <div className="container">
-                <img src="/sponsors/sponsor-template.png" alt="" width={'100%'} />
+                <img src="/sponsors/sponsor-template-02.png" alt="" width={'100%'} />
             </div>
         </section>
     );
