2b9bd5a96e44c5ba952bd3eb2e3de2bca5fc3074
kasrarh <55912648+kasrarh@users.noreply.github.com>
2026-02-13 15:31:49 -0500

    new sponsors banner

--- src/components/LogoSection2.tsx
+++ src/components/LogoSection2.tsx
@@ -3,7 +3,7 @@ export default function LogosSection2() {
     return (
         <section className="sponsors" id="sponsors">
             <div className="container">
-                <img style={{ borderRadius: "20px" }} src="/sponsors/sponsor-template-02.png" alt="" width={'100%'} />
+                <img style={{ borderRadius: "20px" }} src="/sponsors/new-sponsors.png" alt="" width={'100%'} />
             </div>
         </section>
     );
